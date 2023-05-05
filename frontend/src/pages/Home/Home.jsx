dce8cecdc9a70cc3bfb68e62fa9d8126a7a32a0c
guibad22daw <15587039.clot@fje.edu>
2023-05-05 18:46:49 +0200

    S'obté informació del profile de l'usuari

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import useAuth from '../../services/useAuth';
 
 export const Home = () => {
-    const code = new URLSearchParams(window.location.search).get('code');
-    const access_token = useAuth(code);
-    
-    return (
+  const [loading, setLoading] = useState(true)
+  const [profile, setProfile] = useState([]);
+  const [resultatsBusqueda, setResultatsBusqueda] = useState([]);
+  const code = new URLSearchParams(window.location.search).get('code');
+  const access_token = useAuth(code);
+
+  useEffect(() => {
+    async function fetchProfile(access_token) {
+      const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${access_token}` }
+      });
+      setProfile(await result.json());
+      setLoading(false);
+    }
+    fetchProfile(access_token);
+  }, [access_token]);
+
+  const buscarAlbum = async () => {
+    const result = await fetch("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", {
+      method: "GET", headers: { Authorization: `Bearer ${access_token}` }
+    });
+    console.log('acess_token', access_token);
+    setResultatsBusqueda(await result.json());
+  }
+
+  return (
+    loading ? <h1>Loading...</h1> : (
       <div>
         <h1>Benvigut/da :D</h1>
         <h2>Acces token: {access_token}</h2>
+        <h2>Id: {profile.id}</h2>
+        <h2>Email: {profile.email}</h2>
+        <button onClick={() => buscarAlbum()}>Buscar Album</button>
+        <div>{JSON.stringify(resultatsBusqueda)}</div>
       </div>
-    );
+    )
+  );
 }
--- frontend/src/services/useAuth.jsx
+++ frontend/src/services/useAuth.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 const useAuth = code => {
-    const [accessToken, setAccessToken] = useState()
-    const [refreshToken, setRefreshToken] = useState()
-    const [expiresIn, setExpiresIn] = useState()
+    const [accessToken, setAccessToken] = useState();
+    const [refreshToken, setRefreshToken] = useState();
+    const [expiresIn, setExpiresIn] = useState();
 
     useEffect(() => {
         (async () => {
