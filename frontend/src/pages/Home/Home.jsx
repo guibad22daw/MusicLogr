6c33fa555478c48122dfc5241e15ed054ab4d007
guibad22daw <15587039.clot@fje.edu>
2023-05-08 18:43:07 +0200

    provant fetch

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,41 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../services/useAuth';
+import './Home.css';
 
 export const Home = () => {
   const [loading, setLoading] = useState(true)
-  const [profile, setProfile] = useState([]);
   const [accesToken, setAccesToken] = useState(localStorage.getItem('access_token'));
-  const [resultatsBusqueda, setResultatsBusqueda] = useState([]);
+  const [topSongs, setTopSongs] = useState([]);
 
-  useEffect(() => {
-    async function fetchProfile(accesToken) {
-      const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${accesToken}` }
-      });
-      setProfile(await result.json());
-      setLoading(false);
-    }
-    fetchProfile(accesToken);
-  }, [accesToken]);
-
-  const buscarAlbum = async () => {
-    const result = await fetch("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy", {
+  const fetchTopSongs = async (accesToken) => {
+    console.log('accesToken', accesToken)
+    const result = await fetch("https://api.spotify.com/v1/me/top/artists?limit=5", {
       method: "GET", headers: { Authorization: `Bearer ${accesToken}` }
     });
-    console.log('acess_token', accesToken);
-    setResultatsBusqueda(await result.json());
+    setTopSongs(await result.json());
+    console.log('result.json()', result.json())
+    setLoading(false);
   }
+
+  useEffect(() => {
+    fetchTopSongs(accesToken);
+  }, [accesToken]);
 
   return (
     loading ? <h1>Loading...</h1> : (
-      <div>
-        <h1>Benvigut/da :D</h1>
-        <h2>Acces token: {accesToken}</h2>
-        <h2>Id: {profile.id}</h2>
-        <h2>Email: {profile.email}</h2>
-        <button onClick={() => buscarAlbum()}>Buscar Album</button>
-        <div>{JSON.stringify(resultatsBusqueda)}</div>
-      </div>
+      <p>{JSON.stringify(topSongs)}</p>
+      // <div className='container'>
+      //   <div className='top-songs'>
+      //     <h1 className='titol'>Top Songs</h1>
+      //     <div className='songs-container'>
+      //       {
+      //         topSongs.map((song, index) => {
+      //           return (
+      //             <div className='song' key={index}>
+      //               <img className='song-img' src={song.album.images[0].url} alt='Song' />
+      //               <div className='song-info'>
+      //                 <h3 className='song-name'>{song.name}</h3>
+      //                 <h4 className='song-artist'>{song.artists[0].name}</h4>
+      //               </div>
+      //             </div>
+      //           )
+      //         })
+      //       }
+      //     </div>
+      //   </div>
+      // </div>
     )
   );
 }
