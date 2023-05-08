3354028384240daefe01a2f21f26d79ac5a89261
guibad22daw <15587039.clot@fje.edu>
2023-05-08 20:06:25 +0200

    Top 5 cançons es mostren

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -9,11 +9,11 @@ export const Home = () => {
 
   const fetchTopSongs = async (accesToken) => {
     console.log('accesToken', accesToken)
-    const result = await fetch("https://api.spotify.com/v1/me/top/artists?limit=5", {
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=5", {
       method: "GET", headers: { Authorization: `Bearer ${accesToken}` }
     });
-    setTopSongs(await result.json());
-    console.log('result.json()', result.json())
+    const data = await result.json();
+    setTopSongs(data.items);
     setLoading(false);
   }
 
@@ -23,27 +23,26 @@ export const Home = () => {
 
   return (
     loading ? <h1>Loading...</h1> : (
-      <p>{JSON.stringify(topSongs)}</p>
-      // <div className='container'>
-      //   <div className='top-songs'>
-      //     <h1 className='titol'>Top Songs</h1>
-      //     <div className='songs-container'>
-      //       {
-      //         topSongs.map((song, index) => {
-      //           return (
-      //             <div className='song' key={index}>
-      //               <img className='song-img' src={song.album.images[0].url} alt='Song' />
-      //               <div className='song-info'>
-      //                 <h3 className='song-name'>{song.name}</h3>
-      //                 <h4 className='song-artist'>{song.artists[0].name}</h4>
-      //               </div>
-      //             </div>
-      //           )
-      //         })
-      //       }
-      //     </div>
-      //   </div>
-      // </div>
+      <div className='container'>
+        <div className='top-songs'>
+          <h1 className='titol'>Top Songs</h1>
+          <div className='songs-container'>
+            {
+              topSongs.map((song, index) => {
+                return (
+                  <div className='song' key={index}>
+                    <img className='song-img' src={song.album.images[0].url} alt='Song' />
+                    <div className='song-info'>
+                      <h3 className='song-name'>{song.name}</h3>
+                      <h4 className='song-artist'>{song.artists[0].name}</h4>
+                    </div>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     )
   );
 }
