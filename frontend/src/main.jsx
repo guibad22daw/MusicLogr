5aefcc32f661d1f8008d93fd29a95ee7c2368927
guibad22daw <guibad22daw@proton.me>
2023-05-10 01:14:22 +0200

    solucionat error dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { CookiesProvider } from "react-cookie";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CookiesProvider>
       <App />
-    </CookiesProvider>
   </React.StrictMode>,
 )
