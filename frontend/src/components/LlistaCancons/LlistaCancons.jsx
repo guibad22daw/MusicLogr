eee360fcdfc96a7cc55a8a0be56f4df001beaeeb
guibad22daw <15587039.clot@fje.edu>
2023-05-09 18:27:28 +0200

    Llista cançons componetitzades

---
+++ frontend/src/components/LlistaCancons/LlistaCancons.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './LlistaCancons.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+export const LlistaCancons = (props) => {
+    return (
+        <div className='top-songs'>
+            <h2 className='titol'>{props.titol}</h2>
+            <div className='songs-container'>
+                {
+                    props.dades.map((song, index) => {
+                        return (
+                            <div className='song' key={index}>
+                                <div className='song-details'>
+                                    <LazyLoadImage src={song.album.images[0].url} className='song-img' alt="Song" effect="blur"/>
+                                    <div className='song-info'>
+                                        <h3 className='song-name'>{song.name}</h3>
+                                        <h4 className='song-artist'>{song.artists[0].name}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../services/useAuth';
 import './Home.css';
+import { LlistaCancons } from '../../components/LlistaCancons/LlistaCancons';
 
 export const Home = () => {
   const [loading, setLoading] = useState(true);
   const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'));
-  const [topSongs, setTopSongs] = useState([]);
+  const [topUserSongs, setTopUserSongs] = useState([]);
   const [recommendedSongs, setRecommendedSongs] = useState([]);
 
-  const fetchTopSongs = async (accessToken) => {
+  const fetchTopUserSongs = async (accessToken) => {
     const result = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=5", {
       method: "GET",
       headers: { Authorization: `Bearer ${accessToken}` }
     });
     const data = await result.json();
-    setTopSongs(data.items);
+    setTopUserSongs(data.items);
     const topSongIds = data.items.map((song) => song.id).join(',');
     const recommendedResult = await fetch(`https://api.spotify.com/v1/recommendations?limit=5&seed_tracks=${topSongIds}`, {
       method: "GET",
@@ -26,53 +27,19 @@ export const Home = () => {
   };
 
   useEffect(() => {
-    fetchTopSongs(accessToken);
+    fetchTopUserSongs(accessToken);
   }, [accessToken]);
 
   return (
-    loading ? <h1>Loading...</h1> : (
-      <div className='homeContainer'>
-        <div className='top-songs'>
-          <h2 className='titol'>Cançons més escoltades últimament</h2>
-          <div className='songs-container'>
-            {
-              topSongs.map((song, index) => {
-                return (
-                  <div className='song' key={index}>
-                    <div className='song-details'>
-                      <img className='song-img' src={song.album.images[0].url} alt='Song' />
-                      <div className='song-info'>
-                        <h3 className='song-name'>{song.name}</h3>
-                        <h4 className='song-artist'>{song.artists[0].name}</h4>
-                      </div>
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
-        <div className='recommended-songs'>
-          <h2 className='titol'>Cançons recomanades</h2>
-          <div className='songs-container'>
-            {
-              recommendedSongs.map((song, index) => {
-                return (
-                  <div className='song' key={index}>
-                    <div className='song-details'>
-                      <img className='song-img' src={song.album.images[0].url} alt='Song' />
-                      <div className='song-info'>
-                        <h3 className='song-name'>{song.name}</h3>
-                        <h4 className='song-artist'>{song.artists[0].name}</h4>
-                      </div>
-                    </div>
-                  </div>
-                )
-              })
-            }
-          </div>
-        </div>
-      </div>
-    )
+    <div className='homeContainer'>
+      {
+        loading ? <h1>Loading...</h1> : (
+          <>
+            <LlistaCancons dades={topUserSongs} titol="Cançons més escoltades" />
+            <LlistaCancons dades={recommendedSongs} titol="Cançons recomanades" />
+          </>
+        )
+      }
+    </div>
   );
 };
