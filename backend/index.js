2dc69031d6f5c4c90ef9b45174e55530fd43e214
judper22daw <judper22daw2@protonmail.com>
2023-05-23 19:44:06 +0200

    acabat de comentar backend

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,18 @@ import * as bd from "./bd.js";
 const app = express()
 dotenv.config()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors()) // Habilita el CORS per a les sol·licituds HTTP
+app.use(express.json()) // Permet analitzar el cos de les sol·licituds com a JSON
+app.use(express.urlencoded({ extended: true })) 
 
-const PORT = 3001
+const PORT = 3001 // Port en el qual s'escoltarà el servidor
 
+// Endpoint per a la sol·licitud de login
 app.post("/login", async (req, res) => {
+  // Obté el codi d'autorització de la sol·licitud
   const { code } = req.body
+
+  // Crea una nova instància de SpotifyWebApi amb les credencials necessàries
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
@@ -23,19 +27,25 @@ app.post("/login", async (req, res) => {
   })
 
   try {
+    // Realitza una sol·licitud d'autorització a l'API de Spotify utilitzant el codi d'autorització rebut
     const {
       body: { access_token, refresh_token, expires_in },
     } = await spotifyApi.authorizationCodeGrant(code)
 
+    // Retorna l'access token, el refresh token i el temps d'expiració com a resposta en format JSON
     res.json({ access_token, refresh_token, expires_in })
   } catch (err) {
     console.log(err)
-    res.sendStatus(400)
+    res.sendStatus(400) // Retorna un codi d'estat de 400 (Sol·licitud incorrecta) en cas d'error
   }
 })
 
+// Endpoint per a la sol·licitud de refresc de l'access token
 app.post("/refresh", async (req, res) => {
+  // Obté el refresh token de la sol·licitud
   const { refreshToken } = req.body
+
+  // Crea una nova instància de SpotifyWebApi amb les credencials necessàries i el refresh token
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
@@ -44,9 +54,12 @@ app.post("/refresh", async (req, res) => {
   })
 
   try {
+    // Refresca l'access token utilitzant el refresh token
     const {
       body: { access_token, expires_in },
     } = await spotifyApi.refreshAccessToken()
+
+    // Retorna l'access token i el temps d'expiració com a resposta en format JSON
     res.json({ access_token, expires_in })
   } catch (err) {
     console.log(err)
@@ -54,37 +67,37 @@ app.post("/refresh", async (req, res) => {
   }
 })
 
+// Endpoint per desar un usuari a la base de dades
 app.post("/saveUser", async (req, res) => {
   bd.desaUsuariBD(req, res);
 })
 
+// Endpoint per desar un àlbum a la base de dades
 app.post("/saveAlbum", async (req, res) => {
   bd.desaAlbumDB(req, res);
 })
 
+// Endpoint per obtenir els àlbums d'un usuari de la base de dades
 app.get("/getUserAlbums", async (req, res) => {
   bd.obtenirAlbumsBD(req, res);
 })
 
+// Endpoint per afegir un rating a un àlbum a la base de dades
 app.post("/addRating", async (req, res) => {
   bd.desaRatingBD(req, res);
 })
 
+// Endpoint per obtenir tots els ratings de la base de dades
 app.get("/getRatings", async (req, res) => {
   bd.obtenirRatingsBD(req, res);
 })
 
+// Endpoint per obtenir el rating d'un àlbum per a un usuari de la base de dades
 app.get("/getRating", async (req, res) => {
   bd.obtenirRatingBD(req, res);
 })
 
-
-// app.get("/lyrics", async (req, res) => {
-//   const { artist, track } = req.query
-//   const lyrics = (await lyricsFinder(artist, track)) || "No Lyrics Found"
-//   res.json({ lyrics })
-// })
-
+// Escolta les sol·licituds en el port especificat
 app.listen(PORT, err => {
   if (err) console.log(err)
   console.log("listening on port", PORT)
