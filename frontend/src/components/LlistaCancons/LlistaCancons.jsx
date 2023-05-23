fb031d148ae299008961c5a93e502b1099ad0f87
guibad22daw <15587039.clot@fje.edu>
2023-05-23 22:52:41 +0200

    LlistaCancons componetitzat

--- frontend/src/components/LlistaCancons/LlistaCancons.jsx
+++ frontend/src/components/LlistaCancons/LlistaCancons.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './LlistaCancons.css';
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { useNavigate } from 'react-router-dom';
@@ -6,22 +6,78 @@ import { Carregant } from '../Carregant/Carregant';
 
 export const LlistaCancons = (props) => {
     const navigate = useNavigate();
+    const { titol, llista } = props;
+    const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'));
+    const [loading, setLoading] = useState(true);
+    const [topUserSongs, setTopUserSongs] = useState([]);
+    const [recommendedSongs, setRecommendedSongs] = useState([]);
+    const [newReleases, setNewReleases] = useState([]);
+    const [albumMap, setAlbumMap] = useState([]);
+
+    const fetchTopUserSongs = async () => {
+        const result = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=5", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+        const data = await result.json();
+        setTopUserSongs(data.items);
+
+        if (llista == 'songsEscoltades') {
+            setAlbumMap(data.items);
+            setLoading(false);
+        } else if (llista == 'songsRecomenades') {
+            fetchRecommendedSongs(data.items);
+        } else if (llista == 'newReleases') {
+            fetchNewReleases();
+        }
+    };
+
+    const fetchRecommendedSongs = async (data) => {
+        const topSongIds = data.map((song) => song.id).join(',');
+        const recommendedResult = await fetch(`https://api.spotify.com/v1/recommendations?limit=5&seed_tracks=${topSongIds}`, {
+            method: "GET",
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+        const recommendedData = await recommendedResult.json();
+        setRecommendedSongs(recommendedData.tracks);
+        setAlbumMap(recommendedData.tracks);
+        setLoading(false);
+    };
+
+    const fetchNewReleases = async () => {
+        const recommendedResult = await fetch(`https://api.spotify.com/v1/browse/new-releases?country=ES&limit=5`, {
+            method: "GET",
+            headers: { Authorization: `Bearer ${accessToken}` }
+        });
+        const recommendedData = await recommendedResult.json();
+        setNewReleases(recommendedData.albums.items);
+        console.log('recommended :>> ', recommendedData.albums.items);
+        setAlbumMap(recommendedData.albums.items);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        setLoading(true);
+        fetchTopUserSongs();
+    }, [accessToken, llista])
+
 
     return (
         <div className='top-songs'>
             <div className='top-songs-header'>
-                <h2 className='titol'>{props.titol}</h2>
+                <h2 className='titol'>{titol}</h2>
             </div>
             {
-                props.loading ? <Carregant height="340px"/> : (
+                loading ? <Carregant height="340px" /> : (
                     <div className='songs-container'>
                         {
-                            props.dades == undefined || props.length == 0 ? <h2>No hi ha cançons per recomanar</h2> : (
-                                props.dades.map((song, index) => {
+                            albumMap.length == 0 ? <h2>No hi ha cançons per recomanar</h2> : (
+                                albumMap.map((song, index) => {
+                                    console.log('song :>> ', song);
                                     return (
                                         <div className='song shadow-sm' key={index}>
                                             <div className='song-details' onClick={() => navigate(`/album/${song.album.id}`)}>
-                                                <LazyLoadImage src={song.album.images[0].url} width={200} className='song-img' alt="Song" effect="blur" />
+                                                <LazyLoadImage src={song.album == undefined ? song.images[0].url : song.album.images[0].url } width={200} className='song-img' alt="Song" effect="blur" />
                                                 <div className='song-info'>
                                                     <h3 className='song-name'>{song.name}</h3>
                                                     <h4 className='song-artist'>{song.artists[0].name}</h4>
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -5,33 +5,10 @@ import { Separador } from '../../components/Separador';
 import { Carregant } from '../../components/Carregant/Carregant';
 
 const HomePage = () => {
-  const [loading, setLoading] = useState(true);
   const [perfilInfo, setPerfilInfo] = useState(JSON.parse(localStorage.getItem('perfil_info')));
-  const [accessToken, setAccessToken] = useState(localStorage.getItem('access_token'));
-  const [topUserSongs, setTopUserSongs] = useState([]);
-  const [recommendedSongs, setRecommendedSongs] = useState([]);
   const [greeting, setGreeting] = useState('');
 
-  const fetchTopUserSongs = async (accessToken) => {
-    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=5", {
-      method: "GET",
-      headers: { Authorization: `Bearer ${accessToken}` }
-    });
-    const data = await result.json();
-    setTopUserSongs(data.items);
-    const topSongIds = data.items.map((song) => song.id).join(',');
-    const recommendedResult = await fetch(`https://api.spotify.com/v1/recommendations?limit=5&seed_tracks=${topSongIds}`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${accessToken}` }
-    });
-    const recommendedData = await recommendedResult.json();
-    setRecommendedSongs(recommendedData.tracks);
-    setLoading(false);
-  };
-
   useEffect(() => {
-    fetchTopUserSongs(accessToken);
-
     const currentHour = new Date().getHours();
     let greetingText = '';
     if (currentHour >= 6 && currentHour < 12) {
@@ -42,23 +19,21 @@ const HomePage = () => {
       greetingText = 'Bona nit';
     }
     setGreeting(greetingText);
-  }, [accessToken]);
+  }, []);
 
   return (
     <div className='container-xxl homeContainer'>
-      {
-        <>
-          <Separador height="15px" />
-          <div className='titolHome'>
-            <h1>{greeting}, {perfilInfo.display_name}</h1>
-          </div>
-          <Separador />
-          <LlistaCancons dades={topUserSongs} loading={loading} titol="Cançons més escoltades" />
-          <Separador />
-          <LlistaCancons dades={recommendedSongs} loading={loading} titol="Cançons recomanades" />
-          <Separador />
-        </>
-      }
+      <Separador height="15px" />
+      <div className='titolHome'>
+        <h1>{greeting}, {perfilInfo.display_name}</h1>
+      </div>
+      <Separador />
+      <LlistaCancons titol="Cançons més escoltades" llista="songsEscoltades" />
+      <Separador />
+      <LlistaCancons titol="Cançons recomanades" llista="songsRecomenades" />
+      <Separador />
+      <LlistaCancons titol="Artistes recomanats" llista="newReleases" />
+      <Separador />
     </div>
   );
 };
