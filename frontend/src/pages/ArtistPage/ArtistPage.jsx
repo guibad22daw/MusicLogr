255c8feddb866e8f22e574bd85a9a5ff7970a7c2
guibad22daw <guibad22daw@proton.me>
2023-05-23 02:30:01 +0200

    modificat boto filtrar

--- frontend/src/pages/ArtistPage/ArtistPage.jsx
+++ frontend/src/pages/ArtistPage/ArtistPage.jsx
@@ -87,8 +87,8 @@ const ArtistPage = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='artist-filtrarContainer' onClick={() => setShowDropdown(!showDropdown)}>
-                            <BiFilter color='white' size="1.5em" />
+                        <div className='artist-filtrarContainer' onClick={() => setShowDropdown(!showDropdown)} style={showDropdown ? {backgroundColor: "white", color: "black"} : {}}>
+                            <BiFilter style={{color: showDropdown? "black" : "white"}} size="1.5em" />
                             <h5 className='artist-albumsTitle'>Filtrar</h5>
                             <div className="filtrar-div" style={showDropdown ? { visibility: "visible", opacity: 1, top: "2px" } : {}} >
                                 <ul>
